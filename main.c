85856d7414e82b86a9d877233deaa6dd8dbf7cc5
Oleg Silkin <o.silkin98@gmail.com>
2018-09-02 00:15:32 -0500

    Adds testing routines

--- main.c
+++ main.c
@@ -4,10 +4,38 @@
 #include "fastsort.h"
 #include "skiplist.h"
 
+#define N 20
+
+#define printArray(data) for(int i = 0; i < N; ++i) { printf("%d ", data[i]);}
+
+void printArrayFunc(int *data, int n) {
+
+    for(register size_t i = 0; i < n; ++i) {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+}
+
 
 int main(void) {
 
+    int *data = malloc(sizeof(int) * N);
 
+    for(int i = 0; i < N; ++i) {
+        data[i] = rand() % 17;
+        //printf("%d ", data[i]);
+    }
+
+    /**sortData(data);**/
+
+    printf("printArray macro: ");
+    printArray(data)
+
+    sortData(data, N);
+
+    printArray(data);
+
+    free(data);
 
     return 0;
 }
