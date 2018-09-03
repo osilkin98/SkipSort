0fc5cec1a33bea0bcf162b2442dad4af287c82d1
Oleg Silkin <o.silkin98@gmail.com>
2018-09-03 08:33:07 -0500

    Implements radix sort

--- src/skiplist.py
+++ src/skiplist.py
@@ -232,6 +232,41 @@ def quickSort(x):
         return first_part + second_part
 
 
+def radix_sort(alist, base=10):
+    if alist == []:
+        return
+
+    def key_factory(digit, base):
+        def key(alist, index):
+            return ((alist[index] // (base ** digit)) % base)
+
+        return key
+
+    largest = max(alist)
+    exp = 0
+    while base ** exp <= largest:
+        alist = counting_sort(alist, base - 1, key_factory(exp, base))
+        exp = exp + 1
+    return alist
+
+
+def counting_sort(alist, largest, key):
+    c = [0] * (largest + 1)
+    for i in range(len(alist)):
+        c[key(alist, i)] = c[key(alist, i)] + 1
+
+    # Find the last index for each element
+    c[0] = c[0] - 1  # to decrement each element for zero-based indexing
+    for i in range(1, largest + 1):
+        c[i] = c[i] + c[i - 1]
+
+    result = [None] * len(alist)
+    for i in range(len(alist) - 1, -1, -1):
+        result[c[key(alist, i)]] = alist[i]
+        c[key(alist, i)] = c[key(alist, i)] - 1
+
+    return result
+
 
 def test(sort, N=100, a=0, b=maxsize):
     data = [randint(a, b) for i in range(N)]
@@ -256,6 +291,11 @@ if __name__ == '__main__':
     skip_time = timeit.timeit("test(skipSort, N={}, a={}, b={})".format(N, a, b),
                               number=100, setup="from __main__ import test, skipSort")
     print("Skipsort Time: {}secs".format(skip_time))
+
+    skip_time = timeit.timeit("test(radix_sort, N={}, a={}, b={})".format(N, a, b),
+                              number=100, setup="from __main__ import test, radix_sort")
+
+    print("Radix Time: {}secs".format(skip_time))
 
     quickTime = timeit.timeit("test(quickSort, N={}, a={}, b={})".format(N, a, b),
                               number=100, setup="from __main__ import test, quickSort")
