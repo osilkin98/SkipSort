b19c605faf48be54ea3934ba69d60b0ff784251a
Oleg Silkin <o.silkin98@gmail.com>
2018-09-03 05:51:14 -0500

    Removes insertion routine from default constructor

--- src/skiplist.py
+++ src/skiplist.py
@@ -7,39 +7,18 @@ class Skiplist(object):
 
     class SNode(object):
 
-        def __init__(self, key, height, prev_node=None):
+        def __init__(self, key, height):
             """
 
             :param int key: Number to store inside the current node
             :param int height: This is the height of the node which we will create
-            :param SNode prev_node: Previous Node object that instantiated
             """
             self.value = key  # Set the key
             self.count = 1  # Set the current count
             self.height = height    # Set the maximum height, this value should be determined outside
 
             # If we don't have a previous node, then this is the first Node to be inserted
-            if prev_node is None:
-
-                # Height of all entry towers, filled with null pointers
-                self.next = [None] * self.height
-
-            else:
-
-                self.next = [None] * self.height
-
-                traversal_node = prev_node
-
-                for level in range(self.height - 1, -1, -1):
-
-                    # Positions the traversal node to be the node just before the current one in value
-                    while traversal_node.next[level] is not None and traversal_node.next[level].value < self.value:
-                        traversal_node = traversal_node.next[level]
-
-                    # Now the traversal_node is in a position where it's right before the current
-                    # node in terms of key value, and we can just do a linked list insertion
-                    self.next[level] = traversal_node.next[level]
-                    traversal_node.next[level] = self
+            self.next = [None] * self.height
 
 
     ''' Skiplist diagram:
