437f23641460c54418173f831066c49cc5dcff01
Oleg Silkin <o.silkin98@gmail.com>
2018-09-08 15:50:09 -0500

    Modifies sort_with_ranged_bases function to take a list of lengths

--- src/genetic_skiplist.py
+++ src/genetic_skiplist.py
@@ -14,22 +14,29 @@ def skipsort_test(base=2, N=100, a=0, b=maxsize):
     data.clear()
 
 
-def sort_with_ranged_bases(a=-maxsize-1, b=maxsize, n=100, trials=10, start=2.0, stop=8.0, increment=1.0):
+def sort_with_ranged_bases(a=-maxsize-1, b=maxsize, lengths: tuple=None, trials=10, start=2.0, stop=8.0, increment=1.0):
 
     data = []
     base = start
     while base <= stop + increment:
-        print("Running "+str(trials)+" trials for skipsort with a probability base of Pb = " +str(base) +
-              ", on a dataset of N=" +str(n) + "\nwith randomized datasets generated between a = "+
-              str(a)+" and b = "+str(b))
 
-        base_time = timeit(stmt="skipsort_test(base={}, N={}, a={}, b={})".format(base, n, a, b), number=trials,
-                           setup="from __main__ import skipsort_test")
+        # Basic array
+        length_times = [base]
 
-        # x: base, y: time taken to sort data using probability base b
-        data.append([base, base_time])
+        for n in lengths:
+            print("Running " + str(trials) + " trials for skipsort with a probability base of Pb = " + str(base) +
+                  ", on a dataset of N=" + str(n) + "\nwith randomized datasets generated between a = " +
+                  str(a) + " and b = " + str(b))
 
-        print("Time taken: " + str(base_time) + " secs\n")
+            base_time = timeit(stmt="skipsort_test(base={}, N={}, a={}, b={})".format(base, n, a, b), number=trials,
+                               setup="from __main__ import skipsort_test")
+
+            length_times.append(base_time)
+
+            print("Time taken: " + str(base_time) + " secs\n")
+
+        # x: base, [Y]: times taken to sort data using probability base b with variable data sizes
+        data.append(length_times)
 
         base += increment
 
@@ -56,22 +63,46 @@ def sort_with_ranged_data(a=-maxsize-1, b=maxsize, base=1.5, trials=100,
 
         n = n + increment if type.lower() == 'linear' else n * increment
 
+    return np.array(data)
 
 if __name__ == '__main__':
-
-    a, b, n = 0, maxsize, 10000
-    trials = 10
-    start, stop = 0.1, 8.0
-    inc = 0.1
+    a, b, n = 0, 2 ** 31, 1000
+    trials = 100
+    start, stop = 2.0, 4.0
+    inc = 1
 
     data = sort_with_ranged_bases(a=a, b=b, n=n, trials=trials, start=start, stop=stop, increment=inc)
 
     sort_time_series = pd.Series(data=data[:, 1], index=data[:, 0])
 
     plot = sort_time_series.plot(
-        title="Time Taken to Sort {} Members Generated on [{}, {}], {} times".format(n, a, b, trials))
+        title="Time Taken to Sort {} Members Generated on [{}, {}]\n{} Times, Incrementing the Probability Bases with {}".format(
+            n, a, b, trials, inc))
 
     plot.set_xlabel("Probability Bases")
     plot.set_ylabel("Time Taken (seconds)")
 
     plt.show()
+
+    '''
+    a, b, n = 0, 2 ** 31, 1000
+    trials = 100
+    start, stop = 0, 10000
+    inc = 5
+    mode='Geometric'
+    base=1.5
+
+    data = sort_with_ranged_data(a=a, b=b, trials=trials, base=base, start=start, stop=stop, increment=inc, type=mode)
+
+    sort_time_series = pd.Series(data=data[:, 1], index=data[:, 0])
+
+    plot = sort_time_series.plot(
+        title="Time Taken to Sort Random Datasets of Variable Length, \nWith Values Generated On [{}, {}]\
+        \nUsing {} Increments With Probability Base Pb = {}".format(a, b, mode, base))
+
+    plot.set_xlabel("Probability Bases")
+    plot.set_ylabel("Time Taken (seconds)")
+
+    plt.show()
+
+    '''
