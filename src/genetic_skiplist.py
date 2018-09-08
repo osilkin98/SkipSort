8f3734d2ed8a4e84c3f19991d02509b365b5026d
Oleg Silkin <o.silkin98@gmail.com>
2018-09-08 12:59:14 -0500

    Implements function specific for testing skipsort

--- src/genetic_skiplist.py
+++ src/genetic_skiplist.py
@@ -1,6 +1,14 @@
-# from sorting_algorithms import skipSort, test
+from sorting_algorithms import skipSort
 from sys import maxsize
 from timeit import timeit
+from random import randint
+
+
+def skipsort_test(base=2, N=100, a=0, b=maxsize):
+    data = [randint(a, b) for i in range(N)]
+    skipSort(data, base)
+    data.clear()
+
 
 def sort_with_ranged_bases(a=-maxsize-1, b=maxsize, n=100, trials=10, start=2, stop=8, increment=1):
 
@@ -10,12 +18,13 @@ def sort_with_ranged_bases(a=-maxsize-1, b=maxsize, n=100, trials=10, start=2, s
               ", on a dataset of N=" +str(n) + "\nwith randomized datasets generated between a = "+
               str(a)+" and b = "+str(b))
 
-        base_time = timeit(stmt="test(skipSort, N={}, a={}, b={})".format(n, a, b), number=trials,
-                          setup="from sorting_algorithms import skipSort, test")
+        base_time = timeit(stmt="skipsort_test(base={}, N={}, a={}, b={})".format(base, n, a, b), number=trials,
+                           setup="from __main__ import skipsort_test")
 
         print("Time taken: " + str(base_time) + " secs")
 
         base += increment
 
-if __name__ == 'main':
 
+if __name__ == 'main':
+    sort_with_ranged_bases()
