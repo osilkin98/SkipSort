58ceb6c08ba6a59726457dab1da8af1758c81a35
Oleg Silkin <o.silkin98@gmail.com>
2018-09-08 11:05:44 -0500

    Moves radix_sort & counting_sort to sorting_algorithms.py

--- src/skiplist.py
+++ src/skiplist.py
@@ -208,41 +208,6 @@ def skipSort(data: list):
 
 
 
-def radix_sort(alist, base=10):
-    if alist == []:
-        return
-
-    def key_factory(digit, base):
-        def key(alist, index):
-            return ((alist[index] // (base ** digit)) % base)
-
-        return key
-
-    largest = max(alist)
-    exp = 0
-    while base ** exp <= largest:
-        alist = counting_sort(alist, base - 1, key_factory(exp, base))
-        exp = exp + 1
-    return alist
-
-
-def counting_sort(alist, largest, key):
-    c = [0] * (largest + 1)
-    for i in range(len(alist)):
-        c[key(alist, i)] = c[key(alist, i)] + 1
-
-    # Find the last index for each element
-    c[0] = c[0] - 1  # to decrement each element for zero-based indexing
-    for i in range(1, largest + 1):
-        c[i] = c[i] + c[i - 1]
-
-    result = [None] * len(alist)
-    for i in range(len(alist) - 1, -1, -1):
-        result[c[key(alist, i)]] = alist[i]
-        c[key(alist, i)] = c[key(alist, i)] - 1
-
-    return result
-
 
 def test(sort, N=100, a=0, b=maxsize):
     data = [randint(a, b) for i in range(N)]
