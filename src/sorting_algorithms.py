4ffc6678ad4fe053079559900424ace2a5d6070c
Oleg Silkin <o.silkin98@gmail.com>
2018-09-09 04:45:09 -0500

    Implements iterative QuickSort for heuristic comparison

--- src/sorting_algorithms.py
+++ src/sorting_algorithms.py
@@ -25,6 +25,91 @@ def bubbleSort(data):
                 data[i + 1] = temp
 
 
+def partition(arr, low, high):
+    i = (low - 1)  # index of smaller element
+    pivot = arr[high]  # pivot
+
+    for j in range(low, high):
+
+        # If current element is smaller
+        # than or equal to pivot
+        if arr[j] <= pivot:
+            # increment index of
+            # smaller element
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return (i + 1)
+
+
+# Python program for implementation of Quicksort
+
+# This function is same in both iterative and recursive
+def partition(arr, l, h):
+    i = (l - 1)
+    x = arr[h]
+
+    for j in range(l, h):
+        if arr[j] <= x:
+            # increment index of smaller element
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[h] = arr[h], arr[i + 1]
+    return (i + 1)
+
+
+# Function to do Quick sort
+# arr[] --> Array to be sorted,
+# l  --> Starting index,
+# h  --> Ending index
+def quickSortIterative(arr, l=0, h=None):
+    # Create an auxiliary stack
+    h = len(arr) - 1 if h is None else h
+
+    size = h - l + 1
+    stack = [0] * (size)
+
+    # initialize top of stack
+    top = -1
+
+    # push initial values of l and h to stack
+    top = top + 1
+    stack[top] = l
+    top = top + 1
+    stack[top] = h
+
+    # Keep popping from stack while is not empty
+    while top >= 0:
+
+        # Pop h and l
+        h = stack[top]
+        top = top - 1
+        l = stack[top]
+        top = top - 1
+
+        # Set pivot element at its correct position in
+        # sorted array
+        p = partition(arr, l, h)
+
+        # If there are elements on left side of pivot,
+        # then push left side to stack
+        if p - 1 > l:
+            top = top + 1
+            stack[top] = l
+            top = top + 1
+            stack[top] = p - 1
+
+        # If there are elements on right side of pivot,
+        # then push right side to stack
+        if p + 1 < h:
+            top = top + 1
+            stack[top] = p + 1
+            top = top + 1
+            stack[top] = h
+
+# Recursive impleentation
 def quickSort(x):
     if len(x) == 1 or len(x) == 0:
         return x
